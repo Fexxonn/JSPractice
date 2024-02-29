a4635427e0d73515599d1a61efbc2f25fd9f5d8e
Fexxonn <torrenegradavid@gmail.com>
2024-02-29 00:36:29 -0500

    0.3

--- main5.js
+++ main5.js
@@ -1,8 +1,7 @@
-
 for (let i = 0; i < 20; i++) {
-  if (i%2===0) {
-    console.log("El numero ", i , "es par")
-  }else{
-    console.log("El numero ", i , "Es impar")
+  if (i % 2 === 0) {
+    console.log("El numero ", i, "es par");
+  } else {
+    console.log("El numero ", i, "Es impar");
   }
 }
--- main6.js
+++ main6.js
@@ -1,10 +1,8 @@
-let num = 5;
-for (let i = 0; i < num; i++) {
+for (let i = 0; i < 5; i++) {
   let numPisos = "";
   for (let j = 0; j < i + 1; j++) {
     numPisos = numPisos + "*";
-    
   }
 
-  console.log(numPisos)
+  console.log(numPisos);
 }
